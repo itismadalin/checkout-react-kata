c1a777c81637192ade3750a35434ee68557de229
Madalin <madalin.chirila@pumateamwear.com>
2020-06-06 20:50:13 +0100

    Edit Payment page

--- checkout-react-kata/src/components/Payment/Payment.js
+++ checkout-react-kata/src/components/Payment/Payment.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
 import { Link } from "@reach/router";
+import "./Payment.css";
+import Button from "react-bootstrap/Button";
 
 const Payment = () => {
-    return (
-        <div>
-           <p>Thank you for your payment.</p>
-           <p>Please press the button below to go back to the homepage...</p>
-           <Link to="/">
-           <button>DONE</button>
-           </Link>
-        </div>
-    );
+  return (
+    <div className="Payment">
+      <div>
+        <p>Thank you for your payment.</p>
+        <p>Please press the button below to go back to the homepage...</p>
+        <Link to="/">
+          <Button variant="info">Done</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
